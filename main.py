29f87431cc7ec20d4c700d20cfcfecbae1732085
yargol13 <yargol@gmail.com>
2023-12-11 16:38:18 +0000

    report part done

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     print(f"{num_words} words in the document")
     letter_dictionary = lettercounter(text)
     print(letter_dictionary)
+    list_of_characters_report = report(letter_dictionary)
+    for report_element in list_of_characters_report:
+        print(f"The '{report_element['char']}' character was found {report_element['num']} times")
 
 def book_text(path):
     with open(path) as f:
@@ -25,6 +28,20 @@ def lettercounter(input_string):
         else:
             letter_dic[current_char] = 1
     return letter_dic
+
+def sort_on(d):
+    return d["num"]
+
+def report(input_dictionary):
+    input_dict = input_dictionary
+    sorted_list_of_chars = []
+    for inp_dict_element in input_dict:
+        if inp_dict_element.isalpha():
+           # print(f"{inp_dict_element} {input_dict[inp_dict_element]}")
+            sorted_list_of_chars.append({"char" : inp_dict_element, "num" : input_dict[inp_dict_element]})
+    sorted_list_of_chars.sort(reverse = True, key=sort_on)
+    return sorted_list_of_chars
+        
 
 
 main()
