b25cbffc61b9614e5ea6ede787efffb10465920a
yargol13 <yargol@gmail.com>
2023-12-11 16:01:14 +0000

    letter counter part done

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = book_text(book_path)
     num_words = wordcounter(text)
     print(f"{num_words} words in the document")
+    letter_dictionary = lettercounter(text)
+    print(letter_dictionary)
 
 def book_text(path):
     with open(path) as f:
@@ -10,7 +12,19 @@ def book_text(path):
 
 def wordcounter(example_string):
     words = example_string.split()
-    counter = 0
     return len(words)
+
+def lettercounter(input_string):
+    letter_dic = {}
+    string = input_string
+    string = string.lower()
+    for i in range(0,len(string)):
+        current_char = string[i]
+        if current_char in letter_dic:
+            letter_dic[current_char] = letter_dic[current_char] + 1
+        else:
+            letter_dic[current_char] = 1
+    return letter_dic
+
 
 main()
